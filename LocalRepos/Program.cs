bcb5fd7e40c45060db1943f8e8bc8db2c056d708
Malikov Egor <egor122malikov@gmail.com>
2024-11-05 09:25:56 +0300

    first commit

---
+++ LocalRepos/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LocalRepos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
